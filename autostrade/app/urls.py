1c6a2a25c347a8f3b416b4c7f27ca7ed4100709b
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-07-23 10:54:16 +0200

    WIP 1: adding templates, views, urls and assets.

--- autostrade/app/urls.py
+++ autostrade/app/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name='index'),
+    path("licenza/", views.licenza, name='licenza'),
+    path("crediti/", views.crediti, name='crediti'),
+    path("autostrada/", views.autostrada, name='autostrada'),
+    path("casello/", views.casello, name='casello'),
+    path("comune/", views.comune, name='comune')
 ]
