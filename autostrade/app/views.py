506871a49fce73e568f7d66ddf9fe3a899a6f6d6
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-07-24 11:50:24 +0200

    WIP 2: aggiunti alcuni dettagli.

--- autostrade/app/views.py
+++ autostrade/app/views.py
@@ -3,30 +3,30 @@ from django.template import loader
 
 
 def index(request):
-  template = loader.get_template('index.html')
-  return HttpResponse(template.render())
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
 
 
 def autostrada(request):
-  template = loader.get_template('autostrada.html')
-  return HttpResponse(template.render)
+    template = loader.get_template('autostrada.html')
+    return HttpResponse(template.render)
 
 
 def casello(request):
-  template = loader.get_template('casello.html')
-  return HttpResponse(template.render)
+    template = loader.get_template('casello.html')
+    return HttpResponse(template.render)
 
 
 def comune(request):
-  template = loader.get_template('comune.html')
-  return HttpResponse(template.render)
+    template = loader.get_template('comune.html')
+    return HttpResponse(template.render)
 
 
 def licenza(request):
-  template = loader.get_template('licenza.html')
-  return HttpResponse(template.render)
+    template = loader.get_template('licenza.html')
+    return HttpResponse(template.render)
 
 
 def crediti(request):
-  template = loader.get_template('crediti.html')
-  return HttpResponse(template.render)
+    template = loader.get_template('crediti.html')
+    return HttpResponse(template.render)
