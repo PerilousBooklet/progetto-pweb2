6c5a32633c1840659c1d49744f1231729d6a0e38
Luca Foglieni <l.foglieni5@studenti.unibg.it>
2024-07-24 17:15:47 +0200

    Completato collegamento tabelle ed inizio di bootstrap

--- autostrade/app/views.py
+++ autostrade/app/views.py
@@ -1,26 +1,25 @@
 from django.http import HttpResponse
 from django.template import loader
 import app.customlib
-import collections
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def comune(request):
     listacomuni = app.customlib.getDataList("comune")
-    templateTuple = collections.namedtuple("templateTuple", ("codice", "provincia", "nome"))
-    # print (listacomuni)
     context = {"comuni" : listacomuni}
     template = loader.get_template("comune.html")
     return HttpResponse(template.render(context, request))
 
 def casello(request):
-    context = {}
+    listacaselli = app.customlib.getDataList("casello")
+    context = {"caselli" : listacaselli}
     template = loader.get_template("casello.html")
     return HttpResponse(template.render(context, request))
 
 def autostrada(request):
-    context = {}
+    listaautostrade = app.customlib.getDataList("autostrada")
+    context = {"autostrade" : listaautostrade}
     template = loader.get_template("autostrada.html")
     return HttpResponse(template.render(context, request))
 
