caabb8387ac31b392317b37a059831075331052e
Luca Foglieni <l.foglieni5@studenti.unibg.it>
2024-09-12 22:01:42 +0200

    Commenti e refactor di HTML e JS

--- autostrade/static/js/populateModal.js
+++ autostrade/static/js/populateModal.js
@@ -1,20 +1,23 @@
+//richiamata al click dei pulsanti di modifica e di eliminazione.
+//copia i valori della riga corrente e li inserisce nella form del dialog che compare.
 function populateRowModal(codice, provincia, nome) {
-	// console.log(codice + " " + provincia + " " + nome);
 
 	$("#fakeCodiceModal")[0].value = codice;
-	$("#id_codiceModalEdit")[0].value = codice; //Codice
-	$("#id_provinciaModalEdit")[0].value = provincia; //Provincia
-	$("#id_nomeModalEdit")[0].value = nome; //Nome
+	$("#id_codiceModalEdit")[0].value = codice;
+	$("#id_provinciaModalEdit")[0].value = provincia;
+	$("#id_nomeModalEdit")[0].value = nome;
 
-	$("#id_codiceModalDelete")[0].value = codice; //Codice
-	$("#id_provinciaModalDelete")[0].value = provincia; //Provincia
-	$("#id_nomeModalDelete")[0].value = nome; //Nome
+	$("#id_codiceModalDelete")[0].value = codice;
+	$("#id_provinciaModalDelete")[0].value = provincia;
+	$("#id_nomeModalDelete")[0].value = nome;
 
-	$("#fakeCodiceModalDelete")[0].value = codice; //Codice
-	$("#fakeProvinciaModalDelete")[0].value = provincia; //Provincia
-	$("#fakeNomeModalDelete")[0].value = nome; //Nome
+	$("#fakeCodiceModalDelete")[0].value = codice;
+	$("#fakeProvinciaModalDelete")[0].value = provincia;
+	$("#fakeNomeModalDelete")[0].value = nome;
 }
 
+//richiamata al click del pulsante inserisci nella pagina Comune.
+//copia i campi dei filtri e li mette nella form di inserimento.
 function populateInsertModal() {
 	$("#id_codiceModalInsert")[0].value = $("#id_codice")[0].value;
 	$("#id_provinciaModalInsert")[0].value = $("#id_provincia")[0].value;
